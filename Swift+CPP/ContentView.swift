94e5a74268d8a028d23de6eca67755bef694decf
Ibrohim <ibrohim625m@gmail.com>
2022-08-16 15:01:58 +0500

    Initial Commit

---
+++ Swift+CPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift+CPP
+//
+//  Created by Ibrokhim Movlonov on 16/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift+CPP/Swift_CPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_CPPApp.swift
+//  Swift+CPP
+//
+//  Created by Ibrokhim Movlonov on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_CPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
